24177947ccddb97339bf9d586479cc78f8ef5ee8
WolfAURman <furry.wolf.alone13@gmail.com>
2022-10-08 07:48:56 +0000

    daisy: Lineage-ify!

--- lineage_daisy.mk
+++ lineage_daisy.mk
@@ -1,11 +1,11 @@
 #
-# Copyright (C) 2017 The AOSP Project
+# Copyright (C) 2022 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,6 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
@@ -24,24 +27,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 
-# Allow vendor/extra to override any property by setting it first
-$(call inherit-product-if-exists, vendor/extra/product.mk)
-
-# Inherit some VendorExtra stuff.
-$(call inherit-product-if-exists, vendor/ExclusivePack/config.mk)
-
-# Inherit from MiuiCamera
-$(call inherit-product-if-exists, vendor/MiuiCamera/config.mk)
-
 # Inherit from daisy device
 $(call inherit-product, device/xiaomi/daisy/device.mk)
 
-# Inherit some common Xtended stuff.
-$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := daisy
-PRODUCT_NAME := xtended_daisy
+PRODUCT_NAME := lineage_daisy
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A2 Lite
@@ -49,11 +43,3 @@ PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Boot animation
-TARGET_BOOT_ANIMATION_RES := 1080
-
-# Xtended Stuffs
-XTENDED_BUILD_MAINTAINER := TogoFire
-XTENDED_BUILD_TYPE := OFFICIAL
-XTENDED_BUILD_DONATE_URL := https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=PJ3GD9AL6RPBW&source=url
